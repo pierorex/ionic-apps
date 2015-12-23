bc929ebc4ffd4ac7c6cd35deb97833516d472f99
Pierorex <jeanpierohm@gmail.com>
2015-12-23 06:01:58 +0000

    ionic-tutorial (nodejs server) working. Conference (ionic app) working. myApp (ionic app) working.

---
+++ install.sh
@@ -0,0 +1,11 @@
+wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
+sudo apt-get install --yes nodejs
+npm install -g cordova ionic
+ionic serve -p $PORT nolivereload
+# Now go to preview -> preview running application
+
+
+# For Android:
+sudo apt-get install --yes python-software-properties python g++ make ant openjdk-7-jre openjdk-7-jdk
+sudo apt-get remove android-tools-adb android-tools-fastboot
+wget http://dl.google.com/android/android-sdk_r22.6.2-linux.tgz
